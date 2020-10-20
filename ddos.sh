5b0d91caaad1657a43ae04f05048bb51c48592f4
WhoMHw <71681090+WhoMHw@users.noreply.github.com>
2020-10-20 14:44:08 +0700

    Add files via upload

---
+++ ddos.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+clear
+read -p "masukkan ip target : " ip
+read -p "masukkan jumlah port : " port
+read -p "masukkan jumlah paket : " paket
+
+while [[ true ]]; do
+echo "DDOS via termos $ip ya enggak biaa $port goblok $paket"
+done   
